f98d94246bfabd1d5fb408d5b148d6c563cbb255
luiizsilverio <luiiz.silverio@gmail.com>
2022-11-22 20:49:52 -0300

    quotes from API, tweet quote

--- config-example.js
+++ config-example.js
@@ -1,8 +1,9 @@
 // https://api.api-ninjas.com/v1/quotes?category=success&limit=10
+// The Quotes API provides a nearly endless amount of quotes from famous people throughout history.
 
 export const ENV = {
   API_URL: 'https://api.api-ninjas.com/v1/quotes',
   API_KEY: 'xxxxx',
   API_CATEGORY: 'success',
-  API_LIMIT: 10
+  API_LIMIT: 50
 }
--- script.js
+++ script.js
@@ -1,11 +1,27 @@
 import { ENV } from "./config.js";
+import { localQuotes } from "./quotes.js";
+
+const quoteContainer = document.getElementById('quote-container');
+const quoteText = document.getElementById('quote');
+const authorText = document.getElementById('author');
+const twitterBtn = document.getElementById('twitter');
+const newQuoteBtn = document.getElementById('new-quote');
+const loader = document.getElementById('loader');
 
 let apiQuotes = [];
+let viewedQuotes = 0;
+
+// Show Loading
+function loading(show) {
+  loader.hidden = !show;
+  quoteContainer.hidden = show;
+}
 
 // Get quotes from API
-async function getQuotes() {
-  const apiUrl = `${ENV.API_URL}?category=${ENV.API_CATEGORY}&limit=${ENV.API_LIMIT}`;
+async function getQuotesFromAPI(limit) {
+  const apiUrl = `${ENV.API_URL}?category=${ENV.API_CATEGORY}&limit=${limit}`;
 
+  loading(true);
 
   try {
     const response = await fetch(apiUrl, {
@@ -14,19 +30,57 @@ async function getQuotes() {
     });
 
     apiQuotes = await response.json();
+    console.log(`Carregou mais ${limit} citações da API`)
 
-    newQuote();
-    console.log(apiQuotes);
+    apiQuotes.forEach(item => {
+      localQuotes.push({
+        text: item.quote,
+        author: item.author,
+      });
+    });
+
+    loading(false);
   }
   catch (err) {
     console.log(err);
   }
+
+  newQuote();
 }
 
 // Show new quote
 function newQuote() {
+  // Pick a random quote from api-ninja
+  const quote = localQuotes[Math.floor(Math.random() * localQuotes.length)];
+  authorText.textContent = quote.author || "Desconhecido";
 
+  // Check quote length to determine styling
+  if (quote.text.length > 50) {
+    quoteText.classList.add('long-quote');
+  } else {
+    quoteText.classList.remove('long-quote');
+  }
+
+  quoteText.textContent = quote.text;
+  viewedQuotes ++;
+
+  // A cada 50 citações, carrega mais 50 da API
+  if (viewedQuotes % 10 === 0) {
+    getQuotesFromAPI(10);
+  }
 }
 
+// Tweet Quote
+function tweetQuote() {
+  const twitterUrl = `https://twitter.com/intent/tweet?text=${quoteText.textContent} - ${authorText.textContent}`;
+
+  window.open(twitterUrl, '_blank');
+}
+
+
+// Event listeners
+newQuoteBtn.addEventListener('click', newQuote);
+twitterBtn.addEventListener('click', tweetQuote);
+
 // On Load
-getQuotes();
+getQuotesFromAPI();
